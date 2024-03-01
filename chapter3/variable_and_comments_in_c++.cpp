4d3197c67dcbf3fcb8ef17d896abe1022714fea9
Ayush Singh <115815007+ayushsingh119@users.noreply.github.com>
2024-03-01 21:24:55 +0530

    Create variable_and_comments_in_c++.cpp

---
+++ chapter3/variable_and_comments_in_c++.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+// this is how you can comment down any line //
+int main()
+{
+    int sum = 6;
+    cout << "hello world " << sum << endl;
+    return 0;
+}
